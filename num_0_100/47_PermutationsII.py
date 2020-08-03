ff87774a0778bb2e2fe613ccad0ffb68b1002da8
yahuuu <yahuuu@163.com>
2020-08-03 14:25:55 +0800

    dfs+cut

--- num_0_100/47_PermutationsII.py
+++ num_0_100/47_PermutationsII.py
@@ -1,77 +1,54 @@
-# -*- coding:utf-8 -*-
+# coding:utf-8
 # Created data: 20200721
 
 
-# 递归回溯算法
-class Solution_org:
-    def permute(self, nums):  # List[int]) -> List[List[int]]:
-        rt = []
-        n = len(nums)
-        def helper(left):
-            # 出栈点
-            if left == n:
-               rt.append(nums[:])  # copy 记录
-            for i in range(left, len(nums), 1):
-                nums[left], nums[i] = nums[i], nums[left]
-                # 下一个递归点
-                helper(left + 1)
-                # 回溯, 恢复状态
-                nums[left], nums[i] = nums[i], nums[left]
-        helper(0)
-        return rt
-
-# 这是记录路径不交换元素的方式，这样需要维护一个使用表
-# 这里没有剪枝
 class Solution_nocut:
     def permute(self, nums):  # List[int]) -> List[List[int]]:
         n = len(nums)
         if n == 0: return []
+        nums.sort()
         rt = []
 
         def helper(left, path, used):
             if len(path) == n:
                 rt.append(path[:])
             for i in range(0, n):
-                if used[i]: continue
+                if used[i]: continue  # 用过的元素
                 path.append(nums[i])
                 used[i] = True
                 helper(left+1, path, used)
                 path.pop(-1)
                 used[i] = False
         helper(0, [], [False for _ in range(len(nums))] )
+        # [[1, 1, 2], [1, 2, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 1, 1]]
         return rt
 
-
+# 接下来试下剪枝
 class Solution:
     def permute(self, nums):  # List[int]) -> List[List[int]]:
         n = len(nums)
         if n == 0: return []
+        nums.sort()
         rt = []
 
         def helper(left, path, used):
             if len(path) == n:
                 rt.append(path[:])
+                return
             for i in range(0, n):
                 if used[i]: continue
+                if i>0 and used[i-1] and nums[i-1] == nums[i]:  # 和上一位元素同，且上一个元素用过了，剪枝
+                    continue
                 path.append(nums[i])
                 used[i] = True
                 helper(left+1, path, used)
                 path.pop(-1)
                 used[i] = False
         helper(0, [], [False for _ in range(len(nums))] )
+        # [[1, 1, 2], [1, 2, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 1, 1]]
         return rt
 
-nums = [1, 2, 3]
+nums = [1, 2, 1]
 s = Solution()
-rt = s.permute(nums)
-print(rt)
-
-
-"""
-# 39.组合总和
-40. 组合总和 II
-# 46. 全排列
-47. 全排列 II
-78. 子集
-90. 子集 II
-"""
+res = s.permute(nums)
+print(res)
